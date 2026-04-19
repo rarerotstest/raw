b2a2718f21c7b0875a05e4b5e605b7c8c39ef41e
rarerotstest <therealdrixpy@gmail.com>
2026-04-19 16:42:24 -0500

    Add script loading from external sourcezx
    Load a script from a remote URL with authorization token.

--- toothbrush.lua
+++ toothbrush.lua
@@ -1,1 +1,4 @@
-
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/rarerotstest/raw/refs/heads/main/toothache.lua",
+    {["Authorization"] = "token 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"}
+))()
