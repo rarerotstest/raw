be360a5d61d3cc50d00f5083944b40f48782b9bc
rarerotstest <therealdrixpy@gmail.com>
2026-04-19 16:45:06 -0500

    aa
    aa

--- toothbrush.lua
+++ toothbrush.lua
@@ -1,4 +1,1 @@
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/rarerotstest/raw/refs/heads/main/toothache.lua",
-    {["Authorization"] = "token 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"}
-))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rarerotstest/raw/refs/heads/main/toothache.lua?token=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"))()
